871b4b33f73d7e73fb1af902b323003bbea967b8
Rutz13 <ruturajjadhav456@gmail.com>
2023-05-06 23:36:09 +0530

    Authentication middleware

--- controllers/restaurant-controllers/tableController.js
+++ controllers/restaurant-controllers/tableController.js
@@ -1,7 +1,6 @@
 // Imports
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const router = express.Router();
 const Usermodel = require("../../models/user.model");
 const orderDetail = require("../../models/order-details");
 const localStorage = require("local-storage");
@@ -10,38 +9,25 @@ const JWT_SECRET = process.env.JWT_SECRET;
 // View orders on particular table
 
 exports.viewTable = async (req, res) => {
-    var {
-        table_no
-    } = req.params;
 
-    const token = localStorage.get("jwt");
+  var { table_no } = req.params;
 
-    // Authenticate the restaurant
+  // Get restaurant data from database
+  var restaurantId = await Usermodel.findOne({
+    username: req.user.username,
+  });
 
-    jwt.verify(token, JWT_SECRET, async (err, decodedtoken) => {
-        if (err) {
-            res.send(err.message);
-        } else {
-            var user = decodedtoken.username;
+  // Get all the menu items whose status is pending for the particular table number
+  var orderItems = await orderDetail.find({
+    restaurantId: restaurantId.id,
+    status: "Pending",
+    table_no: table_no
+  });
 
-            // Get restaurant data from database
-            var restaurantId = await Usermodel.findOne({
-                username: user
-            });
-
-            // Get all the menu items whose status is pending for the particular table number
-            var orderItems = await orderDetail.find({
-                restaurantId: restaurantId.id,
-                status: "Pending",
-                table_no: table_no,
-            });
-
-            // Render the menu items
-            orderItems.forEach((order) => {
-                order.menu.forEach((dish) => {
-                    console.log(dish.dish);
-                });
-            });
-        }
+  // Render the menu items
+  orderItems.forEach((order) => {
+    order.menu.forEach((dish) => {
+      console.log(dish.dish);
     });
+  });
 };
--- routes/web/viewtableRoutes.js
+++ routes/web/viewtableRoutes.js
@@ -2,10 +2,11 @@
 const express = require("express");
 const router = express.Router();
 const tableController = require("../../controllers/restaurant-controllers/tableController");
+const authMiddleware = require("../../middlewares/authMiddleware");
 
 // Define routes
 
-router.post('/viewtable' , tableController.viewTable);
+router.get('/viewtable' ,authMiddleware.authMiddleware, tableController.viewTable);
 
 
 module.exports = router;
